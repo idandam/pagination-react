afbd607abb884ea247f9a0b8fb3cec02547bc320
idan-grimberg-damri <idan.grimberg.damri@gmail.com>
2022-01-26 12:18:46 +0200

    finished student selection

--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -10,14 +10,20 @@ const Students: React.FC<{
   onSelectStudent: (canDelete: boolean) => void;
 }> = (props) => {
   const [selected, setSelected] = useState<string[]>([]);
-  const { onSelectStudent } = props;
+  const { onSelectStudent, isInEditMode } = props;
 
   useEffect(() => {
-    console.log(selected);
+    // No students to select
     if (selected.length === 0) {
       onSelectStudent(false);
     }
   }, [selected, onSelectStudent]);
+
+  useEffect(() => {
+    if (!isInEditMode) {
+      setSelected([]);
+    }
+  }, [isInEditMode]);
 
   const studentClickHandler = (
     studentId: string,
@@ -26,10 +32,13 @@ const Students: React.FC<{
     if (props.isInEditMode) {
       if (studentWasSelected !== undefined) {
         setSelected((selected) => {
+          // Indicates the first student selection
           if (selected.length === 0) {
             onSelectStudent(true);
           }
+
           let updatedSelected = [...selected];
+
           if (studentWasSelected && !selected.find((id) => id === studentId)) {
             updatedSelected.push(studentId);
           } else {
