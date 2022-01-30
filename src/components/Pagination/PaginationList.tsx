86aa01b7997f1c452f59da7798c4f0b2b3fae5fc
idan-grimberg-damri <idan.grimberg.damri@gmail.com>
2022-01-30 20:41:11 +0200

    sticky-pagination

--- src/components/Pagination/PaginationList.tsx
+++ src/components/Pagination/PaginationList.tsx
@@ -9,59 +9,61 @@ const PaginationList: React.FC<{
   inEditMode: boolean;
 }> = (props) => {
   return (
-    <div className={styles.container}>
-      <button
-        onClick={props.onPreviousPage}
-        disabled={props.inEditMode}
-        className={styles.btn}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles["btn__icon"]}
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M15 19l-7-7 7-7"
-          />
-        </svg>
-      </button>
-      {props.pagesToDisplay.map((page) => (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
         <button
-          key={page}
-          onClick={props.onPageChange}
+          onClick={props.onPreviousPage}
           disabled={props.inEditMode}
-          className={`${styles["btn__page"]} ${
-            page === props.currPage ? styles["btn__page--current"] : ""
-          }`}
+          className={styles.btn}
         >
-          {page}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles["btn__icon"]}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
         </button>
-      ))}
-      <button
-        onClick={props.onNextPage}
-        disabled={props.inEditMode}
-        className={styles.btn}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={styles["btn__icon"]}
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        {props.pagesToDisplay.map((page) => (
+          <button
+            key={page}
+            onClick={props.onPageChange}
+            disabled={props.inEditMode}
+            className={`${styles["btn__page"]} ${
+              page === props.currPage ? styles["btn__page--current"] : ""
+            }`}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          onClick={props.onNextPage}
+          disabled={props.inEditMode}
+          className={styles.btn}
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 5l7 7-7 7"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles["btn__icon"]}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
